911864b8c7626062d1c47d29355f53878428114f
ariosmen <arielosmen@hotmail.com>
2025-01-19 12:21:10 -0300

    ref

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  // app.setGlobalPrefix('api/v1') // Esto es para asignarle a todas las urls el prefijo
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+export default bootstrap();
+
