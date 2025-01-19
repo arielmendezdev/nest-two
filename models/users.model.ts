9268b708aaf74645c2a7afcf425d2889fe56ef1b
ariosmen <arielosmen@hotmail.com>
2025-01-19 11:11:16 -0300

    refactor

--- models/users.model.ts
+++ models/users.model.ts
@@ -1,12 +1,63 @@
-import { Table, Column, Model, DataType } from 'sequelize-typescript';
+// import { Table, Column, Model, DataType } from 'sequelize-typescript';
+// import { UUIDTypes } from 'uuid';
+
+// @Table({ tableName: 'users', timestamps: true })
+// export class User extends Model<User> {
+//   @Column({
+//     type: DataType.UUID,
+//     primaryKey: true,
+//     defaultValue: DataType.UUIDV4
+//   })
+//   id: UUIDTypes;
+
+//   @Column({
+//     type: DataType.STRING,
+//     allowNull: false,
+//   })
+//   firstName: string;
+
+//   @Column({
+//     type: DataType.STRING,
+//     allowNull: false,
+//   })
+//   lastName: string;
+
+//   @Column({
+//     type: DataType.STRING,
+//   })
+//   email: string;
+
+//   @Column({
+//     type: DataType.STRING,
+//   })
+//   phone: string;
+
+//   @Column({
+//     type: DataType.STRING,
+//   })
+//   address: string;
+
+//   @Column({
+//     type: DataType.BOOLEAN,
+//     defaultValue: false,
+//   })
+//   isDeleted: boolean;
+// }
+
 import { UUIDTypes } from 'uuid';
+import {
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
 
 @Table({ tableName: 'users', timestamps: true })
 export class User extends Model<User> {
   @Column({
     type: DataType.UUID,
     primaryKey: true,
-    defaultValue: DataType.UUIDV4
+    defaultValue: DataType.UUIDV4,
   })
   id: UUIDTypes;
 
@@ -24,22 +75,36 @@ export class User extends Model<User> {
 
   @Column({
     type: DataType.STRING,
+    defaultValue: false,
   })
-  email: string;
+  isDeleted: boolean;
 
   @Column({
     type: DataType.STRING,
+    defaultValue: true,
+  })
+  isAvailable: boolean;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  dni: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
   })
   phone: string;
 
   @Column({
     type: DataType.STRING,
   })
-  address: string;
+  email: string;
 
   @Column({
-    type: DataType.BOOLEAN,
-    defaultValue: false,
+    type: DataType.STRING,
   })
-  isDeleted: boolean;
+  userName: string;
+
 }
